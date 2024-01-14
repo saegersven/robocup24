fc706ec62d217542e0967f49c2b6519d3a35a3a7
kraemerlukas314 <lukas.kraemer.2004@gmail.com>
2024-01-14 18:21:01 +0100

    VL53L0X multiplexing

--- src/arduino_firmware/functions.ino
+++ src/arduino_firmware/functions.ino
@@ -1,19 +1,19 @@
 void init_robot() {
   /*
-  Serial.println(" _____ _ _   _____ _ _     ");
-  Serial.println("| __  |_| |_|   __| |_|___ ");
-  Serial.println("| __ -| |  _|   __| | | . |");
-  Serial.println("|_____|_|_| |__|  |_|_|  _|");
-  Serial.println("                      |_|  ");
-  Serial.println("");
-  Serial.println("--- DEBUGGING WINDOW ---");*/
+    Serial.println(" _____ _ _   _____ _ _     ");
+    Serial.println("| __  |_| |_|   __| |_|___ ");
+    Serial.println("| __ -| |  _|   __| | | . |");
+    Serial.println("|_____|_|_| |__|  |_|_|  _|");
+    Serial.println("                      |_|  ");
+    Serial.println("");
+    Serial.println("--- DEBUGGING WINDOW ---");*/
   // store current bat voltage on startup so it does not have to be read out each loop iteration for m() function
 
   start_up_bat_voltage = get_battery_voltage();
 
   pinMode(3, OUTPUT);
   digitalWrite(3, LOW);
-  
+
   // battery is not switched on, don't start moving
   if (start_up_bat_voltage < 5.0) {
     pinMode(13, OUTPUT);
@@ -31,25 +31,17 @@ void init_robot() {
 
   Wire.begin();
 
-  dist_sensors[0].setMeasurementTimingBudget(20000);
-  dist_sensors[0].setTimeout(100);
-  //dist_sensors[0].setAddress(0x8A);
+  init_dist_sensors();
 
-  if(!dist_sensors[0].init()) {
-    panic();
-  }
-
-  dist_sensors[0].startContinuous();
-
-  if(!bno.begin()) {
+  if (!bno.begin()) {
     panic();
   }
 
   /*
-  Serial.print("Start up battery voltage: ");
-  Serial.print(start_up_bat_voltage);
-  Serial.println("V");
-  Serial.println("Starting robot...");*/
+    Serial.print("Start up battery voltage: ");
+    Serial.print(start_up_bat_voltage);
+    Serial.println("V");
+    Serial.println("Starting robot...");*/
 }
 
 // function to drive robot
@@ -57,10 +49,10 @@ void init_robot() {
 // int8_t right    : right motor speed from -127 to 128
 // int16_t duration: duration for how long the motors should turn
 void m(int8_t left, int8_t right, int16_t duration) {
-  if(left > 100) left = 100;
-  if(right > 100) right = 100;
-  if(left < -100) left = -100;
-  if(right < -100) right = -100;
+  if (left > 100) left = 100;
+  if (right > 100) right = 100;
+  if (left < -100) left = -100;
+  if (right < -100) right = -100;
 
   // full stop:
   if (left == 0 && right == 0) {
@@ -73,7 +65,7 @@ void m(int8_t left, int8_t right, int16_t duration) {
     delay(duration);
     return;
   }
-  
+
   // set spinning direction of both motors:
   if (left < 0) {
     digitalWrite(M_LEFT_A, LOW);
@@ -92,22 +84,22 @@ void m(int8_t left, int8_t right, int16_t duration) {
 
   // set pwm signal (aka speed) for both motors:
   uint8_t left_pwm  = (uint8_t)((float)abs(left) * 255.0f / 100.0f);
-  uint8_t right_pwm = (uint8_t)((float)abs(right) * 255.0f / 100.0f); 
-  
+  uint8_t right_pwm = (uint8_t)((float)abs(right) * 255.0f / 100.0f);
+
   // since bat voltage can be as high as 16.8V and we have 12V motors, we need to adjust the duty cycle based on bat voltage
   float normalize_factor = (11.0f / start_up_bat_voltage); // 11V instead of 12V because voltages are still a little too high
   if (normalize_factor > 1.0f) normalize_factor = 1.0f;
   left_pwm *= normalize_factor;
   right_pwm *= normalize_factor;
-  
+
   /*
-  Serial.print("Left: ");
-  Serial.print(left_pwm);
-  Serial.print("  Right: ");
-  Serial.println(right_pwm);*/
+    Serial.print("Left: ");
+    Serial.print(left_pwm);
+    Serial.print("  Right: ");
+    Serial.println(right_pwm);*/
   analogWrite(M_LEFT_EN, left_pwm);
   analogWrite(M_RIGHT_EN, right_pwm);
-  if(duration > 0) {
+  if (duration > 0) {
     delay(duration);
     m(0, 0, 0);
   }
@@ -122,9 +114,9 @@ void turn(int16_t angle_mrad) {
   float angle_rad = (float)angle_mrad / 1000.0f;
   float angle_deg = angle_rad * 180.0f / 3.141592f;
 
-  if(abs(angle_deg) <= 30) {
+  if (abs(angle_deg) <= 30) {
     int duration = powf(abs(angle_rad), 0.8f) * 340.0f;
-    if(angle_deg < 0) {
+    if (angle_deg < 0) {
       m(-60, 60, duration);
     } else {
       m(60, -60, duration);
@@ -134,9 +126,9 @@ void turn(int16_t angle_mrad) {
     return;
   }
 
-  if(angle_deg > 30) angle_deg -= 3;
-  if(angle_deg < -30) angle_deg += 3;
-  if(angle_deg == 0) return;
+  if (angle_deg > 30) angle_deg -= 3;
+  if (angle_deg < -30) angle_deg += 3;
+  if (angle_deg == 0) return;
 
   int min_duration = MIN_TIME_PER_DEG * abs(angle_deg);
   int max_duration = MAX_TIME_PER_DEG * abs(angle_deg);
@@ -145,10 +137,10 @@ void turn(int16_t angle_mrad) {
   float current_heading = get_heading() * 180.0f / PI;
   float final_heading = current_heading + angle_deg;
 
-  if(final_heading > 360.0f) final_heading -= 360.0f;
-  if(final_heading < 0.0f) final_heading += 360.0f;
+  if (final_heading > 360.0f) final_heading -= 360.0f;
+  if (final_heading < 0.0f) final_heading += 360.0f;
 
-  if(angle_deg < 0) {
+  if (angle_deg < 0) {
     m(-60, 60, 0);
   } else {
     m(60, -60, 0);
@@ -156,12 +148,12 @@ void turn(int16_t angle_mrad) {
 
   long long start_time = millis();
 
-  while(millis() - start_time < min_duration);
-  while(millis() - start_time < max_duration) {
+  while (millis() - start_time < min_duration);
+  while (millis() - start_time < max_duration) {
     update_orientation();
     float heading = get_heading() * 180.0f / PI;
 
-    if(abs(heading - final_heading) < TURN_TOLERANCE) break;
+    if (abs(heading - final_heading) < TURN_TOLERANCE) break;
   }
 
   m(0, 0, 50);
@@ -170,28 +162,21 @@ void turn(int16_t angle_mrad) {
 }
 
 void servo(uint8_t id, uint8_t angle, bool stall, bool nodelay) {
-  /*Serial.print("Moving servo: ");
-  Serial.print(id);
-  Serial.print(" angle: ");
-  Serial.print(angle);
-  Serial.print(" stall?: ");
-  Serial.println(stall);*/
-  
-  if(angle == 0) {
+  if (angle == 0) {
     // Toggle attachmeant of servo
-    if(servos[id].attached() && !stall) servos[id].detach();
-    else if(stall) servos[id].attach(servo_pins[id]);
+    if (servos[id].attached() && !stall) servos[id].detach();
+    else if (stall) servos[id].attach(servo_pins[id]);
     return;
   }
 
-  if(!servos[id].attached()) servos[id].attach(servo_pins[id]);
+  if (!servos[id].attached()) servos[id].attach(servo_pins[id]);
 
   int prev_angle = servos[id].read();
   servos[id].write(angle);
 
-  if(!nodelay) delay(abs(angle - prev_angle) * 8);
+  if (!nodelay) delay(abs(angle - prev_angle) * 8);
 
-  if(!stall) servos[id].detach();
+  if (!stall) servos[id].detach();
 }
 
 void update_orientation() {
@@ -211,13 +196,62 @@ float get_pitch() {
   return pitch;
 }
 
-int16_t distance(int sensor_id) {
-  int16_t dist = dist_sensors[sensor_id].readRangeContinuousMillimeters();
-  if(dist_sensors[sensor_id].timeoutOccurred()) return 42;
+// inits all our VL53L0X distance sensors
+// shift register is used for multiplexing
+// logic inside function:
+// 1. power off all sensors
+// 2. power on 1. sensor
+// 3. change I2C address of 1. sensor
+// 4. power on 1. and 2. sensor
+// 4. change I2C address of 2. sensor
+// ...
 
-  if(dist < 0) dist = 0;
-  if(dist > 2000) dist = 2000;
-  
+void init_dist_sensors() {
+  pinMode(PIN_SHIFT_REGISTER_LATCH, OUTPUT);
+  pinMode(PIN_SHIFT_REGISTER_CLOCK, OUTPUT);
+  pinMode(PIN_SHIFT_REGISTER_DATA, OUTPUT);
+
+  bool init_successfull = true;
+  int d = 10; // is increased after init fail
+
+  do {
+    // power off all sensors
+    digitalWrite(PIN_SHIFT_REGISTER_LATCH, LOW);
+    shiftOut(PIN_SHIFT_REGISTER_DATA, PIN_SHIFT_REGISTER_CLOCK, MSBFIRST, B00000000);
+    digitalWrite(PIN_SHIFT_REGISTER_LATCH, HIGH);
+    delay(d);
+
+    for (int i = 0; i < NUM_DIST_SENSORS; ++i) {
+      digitalWrite(PIN_SHIFT_REGISTER_LATCH, LOW);
+      shiftOut(PIN_SHIFT_REGISTER_DATA, PIN_SHIFT_REGISTER_CLOCK, MSBFIRST, dist_sensors_bitmasks[i]);
+      digitalWrite(PIN_SHIFT_REGISTER_LATCH, HIGH);
+      delay(d);
+      if (!dist_sensors[i].init()) {
+        init_successfull = false;
+      } else {
+        dist_sensors[i].setAddress(dist_sensors_addresses[i]);
+        dist_sensors[i].setMeasurementTimingBudget(20000);
+        dist_sensors[i].setTimeout(100);
+      }
+      delay(d);
+    }
+    d = d + 10;
+  } while (init_successfull == false or d > 200);
+}
+
+void write_to_shift_register(byte data) {
+  digitalWrite(PIN_SHIFT_REGISTER_LATCH, LOW);
+  shiftOut(PIN_SHIFT_REGISTER_DATA, PIN_SHIFT_REGISTER_CLOCK, MSBFIRST, data);
+  digitalWrite(PIN_SHIFT_REGISTER_LATCH, HIGH);
+}
+
+int16_t distance(int sensor_id) {
+  int16_t dist = dist_sensors[sensor_id].readRangeSingleMillimeters();
+  if (dist_sensors[sensor_id].timeoutOccurred()) return 10000;
+
+  if (dist < 0) dist = 0;
+  if (dist > 2000) dist = 2000;
+
   return dist;
 }
 
@@ -227,7 +261,7 @@ float get_battery_voltage() {
 }
 
 void panic() {
-  while(true) {
+  while (true) {
     digitalWrite(13, HIGH);
     delay(200);
     digitalWrite(13, LOW);
